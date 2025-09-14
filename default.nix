14b2285c543ef1fafc1fe34085af4e2c0563b67f
Carl Andersson <git@lillecarl.com>
2025-09-14 22:25:42 +0200

    Implement writeExeclineBin
    Wraps writeScriptBin with execlineb shebang

--- default.nix
+++ default.nix
@@ -34,6 +34,14 @@ let
               passAsFile = passAsFileNames;
             }
           ) (builtins.concatStringsSep "\n" commands);
+
+        writeExeclineBin =
+          name: script:
+          pkgs.writeScriptBin name # execline
+            ''
+              #! ${lib.getExe' pkgs.execline "execlineb"}
+              ${script}
+            '';
       })
     ];
   };
